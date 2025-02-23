c135a6bf415e1ea528d6bf81a02fc30a4b32358b
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-02-23 13:06:01 -0600

    chore: setup client folder structure and config

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/test');
+        setData(response.data.message);
+      } catch (err) {
+        console.error(`Error fetching data: ${err.message}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <h1>{data || "Loading..."}</h1>;;
+}
+
+export default App;
