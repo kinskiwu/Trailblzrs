386a41c1ca9b7392f725ac4fd8d27fe75e248ecb
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-02-24 17:56:22 -0600

    feature: pagination ui

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pagination = () => {
+  return (
+    <div className='pagination'>
+      <button className='page-button'>&lt;</button>
+      <button className='page-button'>&gt;</button>
+    </div>
+  );
+};
+
+export default Pagination;
--- client/src/components/ParksList.jsx
+++ client/src/components/ParksList.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import ParkCard from './ParkCard';
+import Pagination from './Pagination';
 import { mockParks } from '../mocks/mockParks';
 
 const ParksList = () => {
   const [parks, setParks] = useState(mockParks);
 
   return (
-    <div className='park-list'>
-      {parks.map((park) => (
-        <ParkCard
-          key={park.parkId}
-          parkId={park.parkId}
-        />
-      ))}
-    </div>
+    <>
+      <div className='park-list'>
+        {parks.map((park) => (
+          <ParkCard
+            key={park.parkId}
+            parkId={park.parkId}
+          />
+        ))}
+      </div>
+      <Pagination />
+    </>
   );
 };
 
