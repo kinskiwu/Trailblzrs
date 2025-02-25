aa8344c50576146b9ba98053221c1a2dbac9f92c
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-02-25 02:41:30 -0600

    refactor: improve trip schema

--- server/src/models/tripSchema.js
+++ server/src/models/tripSchema.js
@@ -4,15 +4,38 @@ const { Schema } = mongoose;
 
 const tripSchema = new Schema(
   {
-    tripId: { type: String, unique: true },
+    tripId: {
+      type: String,
+      unique: true,
+    },
     tripDetails: [
       {
-        date: { type: Date, required: true },
-        parks: [{ type: String, ref: 'Park', required: true }],
+        date: {
+          type: String,
+        },
+        parkDetails: [
+          {
+            parkId: { type: String },
+            parkName: { type: String },
+            state: { type: String },
+            npsLink: { type: String },
+            directions: { type: String },
+          },
+        ],
+        forecastDetails: [
+          {
+            parkId: { type: String },
+            high: { type: Number },
+            low: { type: Number },
+            weather: { type: String },
+            windSpeed: { type: String },
+          },
+        ],
       },
     ],
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
-
 export const Trip = mongoose.model('Trip', tripSchema);
