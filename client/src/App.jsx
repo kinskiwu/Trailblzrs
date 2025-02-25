eb1f1088662622505d3c7b3675b7d56e89f5c555
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-02-24 23:00:34 -0600

    feature: basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css';
 import Header from './components/Header';
+import Parks from './pages/Parks';
+import Trips from './pages/Trips';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <main className='main-content'>
-      </main>
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <main className='main-content'>
+          <Routes>
+            <Route
+              path='/'
+              element={<Parks />}
+            />
+            <Route
+              path='/parks'
+              element={<Parks />}
+            />
+            <Route
+              path='/trips'
+              element={<Trips />}
+            />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
