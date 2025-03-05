546a6eb77306f9ed9e09330da0658428909a0562
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-03-05 09:06:24 -0600

    refactor: move mockParks to mocks folder

--- client/__tests__/Parks.integration.jsx
+++ client/__tests__/Parks.integration.jsx
@@ -3,32 +3,14 @@ import { BrowserRouter } from 'react-router';
 import axios from 'axios';
 import Parks from '../src/pages/Parks';
 import ParksProvider from '../src/contexts/ParksContext';
+import { mockParks } from '../__mocks__/mockParks';
 
-const mockParks = [
-  {
-    parkId: 'C62B89CF-F912-46F4-878C-8EEBD843CEC6',
-    image:
-      'https://www.nps.gov/common/uploads/structured_data/0B28E77A-B721-B2D1-7549D561D8CE4722.jpg',
-    name: 'Dayton Aviation Heritage National Historical Park',
-    city: 'Dayton',
-    state: 'OH',
-    description:
-      'Dayton Aviation Heritage National Historical Park was established to honor the lives and achievements of poet and author Paul Laurence Dunbar and aviation pioneers Wilbur and Orville Wright.',
-    activities: ['Biking', 'Road Biking', 'Guided Tours'],
-    historicalRelevance: ['African American Heritage', 'Aviation'],
-    npsLink: 'https://www.nps.gov/daav',
-    directions: 'http://www.nps.gov/daav/planyourvisit/directions.htm',
-  },
-];
-
-// Create mock functions for the context
 const mockAddParkSelection = jest.fn();
 const mockRemoveParkSelection = jest.fn();
 
-// Mock axios
 jest.mock('axios');
 
-// Mock the ParksContext
+// Mock ParksContext
 jest.mock('../src/contexts/ParksContext', () => {
   return {
     __esModule: true,
